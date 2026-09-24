1cbcd058f71c35ad37f5e5e345ea763c221c8b98
Aaron Truong <115338511+aarondtrng@users.noreply.github.com>
2026-09-23 23:03:20 -0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        first = 0
+        filtered = "".join(char.lower() for char in s if char.isalnum())
+        last = len(filtered)-1
+
+        while(first < last):
+            if filtered[first] != filtered[last]:
+                print(last)
+                print(first)
+                return False
+            first+=1
+            last-=1
+        return True
